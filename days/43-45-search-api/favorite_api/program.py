963caa1634d790988db2e5d9ef2f7125481f1685
Constantin Kraft <ckraft@smail.uni-koeln.de>
2019-03-21 20:16:51 +0100

    Optionally open result in system's default browser

--- days/43-45-search-api/favorite_api/program.py
+++ days/43-45-search-api/favorite_api/program.py
@@ -1,11 +1,20 @@
 """Small script to query the TalkPython search API"""
 import api
+import webbrowser
 
+
+SITE_URL = "https://talkpython.fm"
 
 if __name__ == "__main__":
     query = input("Enter a search term: ")
-
+    # replace white-space by dash == boolean &
+    query = query.replace(" ", "-")
     results = api.search(query)
     print(f"Got {len(results)} results:")
-    for r in results:
-        print(r.title)
+    for i,r in enumerate(results):
+        print(f"[{i}] {r.title}")
+    
+    episode_index = int(input("Which one of these do you want to open? Enter a number: "))
+    url_to_open = SITE_URL + results[episode_index].url
+    webbrowser.open(url_to_open, 2)
+    
